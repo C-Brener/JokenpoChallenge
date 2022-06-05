c4aa9f2927ecb772a6c6c74b734d63b381884cae
CIANDT\caiquebrener <brenercaique0806@gmail.com>
2022-06-05 14:28:02 -0300

    Setup new Toolbar

--- app/src/main/java/com/ciandt/jokenpochallengeebac/presentation/initialscreen/MainActivity.kt
+++ app/src/main/java/com/ciandt/jokenpochallengeebac/presentation/initialscreen/MainActivity.kt
@@ -3,10 +3,20 @@ package com.ciandt.jokenpochallengeebac.presentation.initialscreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ciandt.jokenpochallengeebac.R
+import com.ciandt.jokenpochallengeebac.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setupActionBar()
+        setContentView(binding.root)
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = getString(R.string.app_name_initial_screen)
     }
 }
+
