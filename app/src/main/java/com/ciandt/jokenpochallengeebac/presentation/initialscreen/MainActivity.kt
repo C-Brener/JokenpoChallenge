f0ca1edad1f7eb197c751027d384533bffad005c
CIANDT\caiquebrener <brenercaique0806@gmail.com>
2022-06-05 14:04:17 -0300

    Jokenpo

---
+++ app/src/main/java/com/ciandt/jokenpochallengeebac/presentation/initialscreen/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ciandt.jokenpochallengeebac.presentation.initialscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ciandt.jokenpochallengeebac.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
