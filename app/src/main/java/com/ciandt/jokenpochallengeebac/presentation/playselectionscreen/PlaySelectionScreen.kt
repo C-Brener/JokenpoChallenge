ffb6522a79eab54264c57535679d5201bd14535d
CIANDT\caiquebrener <brenercaique0806@gmail.com>
2022-06-13 23:07:44 -0300

    Create result activity

--- app/src/main/java/com/ciandt/jokenpochallengeebac/presentation/playselectionscreen/PlaySelectionScreen.kt
+++ app/src/main/java/com/ciandt/jokenpochallengeebac/presentation/playselectionscreen/PlaySelectionScreen.kt
@@ -1,16 +1,58 @@
 package com.ciandt.jokenpochallengeebac.presentation.playselectionscreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.ciandt.jokenpochallengeebac.R
 import com.ciandt.jokenpochallengeebac.databinding.ActivityMainBinding
 import com.ciandt.jokenpochallengeebac.databinding.ActivityPlaySelectionScreenBinding
+import com.google.android.material.snackbar.Snackbar
 
 class PlaySelectionScreen : AppCompatActivity() {
-    private lateinit var  binding: ActivityPlaySelectionScreenBinding
+    private lateinit var binding: ActivityPlaySelectionScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlaySelectionScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        spinner()
+        bottomNavigationMovement()
+
+    }
+
+    private fun spinner() {
+        val spinner: Spinner = binding.spinner
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.jogadas,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+    }
+
+    private fun bottomNavigationMovement() {
+        val bottomNavigationView = binding.bottomNavigation
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.player_one -> {
+                    navigationIntent()
+                    true
+                }
+                else -> {
+                    Snackbar.make(binding.root, "Result Screen", Snackbar.LENGTH_SHORT).show()
+                    true
+                }
+            }
+
+        }
+    }
+
+    private fun navigationIntent() {
+        val intent = Intent(this, PlaySelectionScreen::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/ciandt/jokenpochallengeebac/presentation/result/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.ciandt.jokenpochallengeebac.presentation.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ciandt.jokenpochallengeebac.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
