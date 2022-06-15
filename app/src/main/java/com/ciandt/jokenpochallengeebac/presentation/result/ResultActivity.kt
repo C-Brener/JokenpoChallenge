73165fd5e49cf5e278cf7d55d3de929f76f83027
CIANDT\caiquebrener <brenercaique0806@gmail.com>
2022-06-14 23:25:46 -0300

    Implement navigationDrawer

--- app/src/main/java/com/ciandt/jokenpochallengeebac/presentation/result/ResultActivity.kt
+++ app/src/main/java/com/ciandt/jokenpochallengeebac/presentation/result/ResultActivity.kt
@@ -1,19 +1,56 @@
 package com.ciandt.jokenpochallengeebac.presentation.result
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.ciandt.jokenpochallengeebac.R
 import com.ciandt.jokenpochallengeebac.databinding.ActivityResultBinding
+import com.ciandt.jokenpochallengeebac.presentation.initialscreen.MainActivity
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 
 class ResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResultBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navDrawer: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        setupToolbar()
         setupClickListener()
+        drawerLayout = binding.root
+        navDrawer = binding.navigationView
+        setupDrawer()
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        drawerLayout.openDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actionbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.home_option -> {
+                val intent = Intent(this, MainActivity::class.java)
+                navigateUpTo(intent)
+                true
+            }
+            else -> {
+                false
+            }
+        }
     }
 
     private fun setupClickListener() {
@@ -26,21 +63,44 @@ class ResultActivity : AppCompatActivity() {
     }
 
     private fun bottomNavigationMovement(item: MenuItem) {
-        when (item.itemId){
-            R.id.player_one->{
+        when (item.itemId) {
+            R.id.player_one -> {
                 playScreenStart()
             }
-            R.id.result->{
+            R.id.result -> {
                 resultScreenStart()
             }
         }
     }
+
     private fun playScreenStart() {
-       onBackPressed()
+        onBackPressed()
     }
 
-    private fun resultScreenStart(){
+    private fun resultScreenStart() {
         Snackbar.make(binding.root, "Você já está na tela de Resultado", Snackbar.LENGTH_SHORT)
             .show()
     }
+
+    private fun setupToolbar() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_menu)
+    }
+
+    private fun setupDrawer() {
+        navDrawer.setNavigationItemSelectedListener { menuItem ->
+            drawerLayout.closeDrawers()
+            when (menuItem.itemId) {
+                R.id.player_one_navigation_drawer -> {
+                    onBackPressed()
+                    true
+                }
+                else -> {
+                    resultScreenStart()
+                    true
+                }
+            }
+        }
+    }
+
 }
